fe037c601f82c1daa5d86824c6e88952b7966df9
Greg French <gregfrench26@gmail.com>
2017-06-18 16:19:56 -0400

    Updated tests

---
+++ test/unit/specs/Editor.spec.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Editor from '@/components/Editor'
+
+describe('Editor.vue', () => {
+  describe('check logo for correct contents', function() {
+    it('should render correct contents', () => {
+      const Constructor = Vue.extend(Editor)
+      const vm = new Constructor().$mount()
+      expect(vm.$el.querySelector('.logo').textContent)
+        .to.equal('GSAP Editor')
+    })
+  });
+
+  describe('check element sidebar for element .el', function() {
+    it('should render .el', () => {
+      const Constructor = Vue.extend(Editor)
+      const vm = new Constructor().$mount()
+      expect(vm.$el.querySelector('.element-sidebar > .element-sidebar-inner > .element-item').textContent.trim()).to.equal('.el')
+    })
+  });
+});
